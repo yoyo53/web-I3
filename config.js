08dfb70bde0423743e115efd97e73bac7866f3ae
Yohan Villiers <94387950+yoyo53@users.noreply.github.com>
2024-11-12 09:29:20 +0100

    fix front deployment

---
+++ config.js
@@ -0,0 +1,6 @@
+let config = {
+    api_url: "https://web-i3-back.fly.dev/",
+    base_url: "/web-I3/"
+};
+
+export { config }
--- front/vite.config.js
+++ front/vite.config.js
@@ -3,9 +3,11 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { config } from './config.js'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? config.base_url : '/',
   plugins: [
     vue(),
     vueDevTools(),
