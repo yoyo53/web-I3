0ad74ac44d15f84c98f7c40fd5a2d4e3276a2b68
Hugo Parmentier <hugo.parmentier@efrei.net>
2024-11-13 16:20:20 +0100

    maj init db

--- back/database/db_init.sql
+++ back/database/db_init.sql
@@ -1,26 +1,27 @@
 -- DROP ALL TABLES
--- DROP TABLE IF EXISTS answer_questions;
--- DROP TABLE IF EXISTS survey_answers;
--- DROP TABLE IF EXISTS options;
--- DROP TABLE IF EXISTS questions;
--- DROP TABLE IF EXISTS question_type;
--- DROP TABLE IF EXISTS surveys;
--- DROP TABLE IF EXISTS subjects;
--- DROP TABLE IF EXISTS students_classes;
--- DROP TABLE IF EXISTS classes;
--- DROP TABLE IF EXISTS teachers;
--- DROP TABLE IF EXISTS students;
--- DROP TABLE IF EXISTS admins;
--- DROP TABLE IF EXISTS users;
-
+-- DROP TABLE IF EXISTS answer_questions CASCADE ;
+-- DROP TABLE IF EXISTS survey_answers CASCADE;
+-- DROP TABLE IF EXISTS options CASCADE;
+-- DROP TABLE IF EXISTS questions CASCADE;
+-- DROP TABLE IF EXISTS question_type CASCADE;
+-- DROP TABLE IF EXISTS surveys CASCADE;
+-- DROP TABLE IF EXISTS survey_templates CASCADE;
+-- DROP TABLE IF EXISTS students_classes CASCADE;
+-- DROP TABLE IF EXISTS classes CASCADE;
+-- DROP TABLE IF EXISTS teachers CASCADE;
+-- DROP TABLE IF EXISTS students CASCADE;
+-- DROP TABLE IF EXISTS admins CASCADE;
+-- DROP TABLE IF EXISTS users CASCADE;
+-- DROP TABLE IF EXISTS modules CASCADE;
+-- DROP TABLE IF EXISTS subjects CASCADE;
 
 -- Création de la table users
 CREATE TABLE IF NOT EXISTS users (
     userID SERIAL PRIMARY KEY,
-    name VARCHAR(25),
-    firstname VARCHAR(25),
-    email VARCHAR(50) UNIQUE,
-    hashed_password VARCHAR(255)
+    name VARCHAR(25) NOT NULL,
+    firstname VARCHAR(25) NOT NULL,
+    email VARCHAR(50) UNIQUE NOT NULL,
+    hashed_password VARCHAR(255) NOT NULL
 );
 
 -- Création de la table admins, héritant de users
@@ -32,14 +33,14 @@ CREATE TABLE IF NOT EXISTS admins (
 -- Création de la table students, héritant de users
 CREATE TABLE IF NOT EXISTS students (
     studentID SERIAL PRIMARY KEY,
-    student_number INT UNIQUE,
+    student_number INT UNIQUE NOT NULL,
     FOREIGN KEY (studentID) REFERENCES users(userID)
 );
 
 -- Création de la table teachers, héritant de users
 CREATE TABLE IF NOT EXISTS teachers (
     teacherID SERIAL PRIMARY KEY,
-    teacher_number INT UNIQUE,
+    teacher_number INT UNIQUE NOT NULL,
     FOREIGN KEY (teacherID) REFERENCES users(userID)
 );
 
@@ -51,8 +52,8 @@ CREATE TABLE IF NOT EXISTS classes (
 
 -- Création de la table students_classes pour la relation entre les étudiants et les classes
 CREATE TABLE IF NOT EXISTS students_classes (
-    studentID INT,
-    classID INT,
+    studentID INT NOT NULL,
+    classID INT NOT NULL,
     FOREIGN KEY (studentID) REFERENCES students(studentID),
     FOREIGN KEY (classID) REFERENCES classes(classID)
 );
@@ -63,22 +64,37 @@ CREATE TABLE IF NOT EXISTS subjects (
     name VARCHAR(50)
 );
 
--- Création de la table surveys
-CREATE TABLE IF NOT EXISTS surveys (
-    surveyID SERIAL PRIMARY KEY,
-    teacherID INT,
-    classID INT,
-    subjectID INT,
+-- Création de la table modules
+CREATE TABLE IF NOT EXISTS modules (
+    moduleID SERIAL PRIMARY KEY,
+    teacherID INT NOT NULL,
+    classID INT NOT NULL,
+    subjectID INT NOT NULL,
     FOREIGN KEY (teacherID) REFERENCES teachers(teacherID),
     FOREIGN KEY (classID) REFERENCES classes(classID),
     FOREIGN KEY (subjectID) REFERENCES subjects(subjectID),
     UNIQUE (teacherID, classID, subjectID)
 );
 
+-- Création de la table survey_templates
+CREATE TABLE IF NOT EXISTS survey_templates (
+    survey_templateID SERIAL PRIMARY KEY
+);
+
+-- Création de la table surveys
+CREATE TABLE IF NOT EXISTS surveys (
+    surveyID SERIAL PRIMARY KEY,
+    moduleID INT NOT NULL,
+    survey_templateID INT NOT NULL,
+    FOREIGN KEY (moduleID) REFERENCES modules(moduleID),
+    FOREIGN KEY (survey_templateID) REFERENCES survey_templates(survey_templateID)
+);
+
+
 -- Création de la table question_type
 CREATE TABLE IF NOT EXISTS question_type (
     question_typeID SERIAL PRIMARY KEY, 
-    question_type VARCHAR(50) UNIQUE
+    question_type VARCHAR(50) UNIQUE NOT NULL
 );
 
 INSERT INTO question_type (question_type) VALUES ('text'), ('radio'), ('checkbox'), ('score') ON CONFLICT DO NOTHING;
@@ -86,17 +102,17 @@ INSERT INTO question_type (question_type) VALUES ('text'), ('radio'), ('checkbox
 -- Création de la table questions
 CREATE TABLE IF NOT EXISTS questions (
     questionID SERIAL PRIMARY KEY,
-    question_typeID INT,
-    surveyID INT,
+    question_typeID INT NOT NULL,
+    survey_templateID INT NOT NULL,
     question_text TEXT,
-    FOREIGN KEY (surveyID) REFERENCES surveys(surveyID),
+    FOREIGN KEY (survey_templateID) REFERENCES survey_templates(survey_templateID),
     FOREIGN KEY (question_typeID) REFERENCES question_type(question_typeID)
 );
 
 -- Création de la table options pour les choix de réponses
 CREATE TABLE IF NOT EXISTS options (
-    optionID INT,
-    questionID INT,
+    optionID INT NOT NULL,
+    questionID INT NOT NULL,
     option_text TEXT,
     FOREIGN KEY (questionID) REFERENCES questions(questionID),
     PRIMARY KEY (optionID, questionID)
@@ -105,17 +121,17 @@ CREATE TABLE IF NOT EXISTS options (
 -- Création de la table survey_answers pour stocker les réponses aux enquêtes
 CREATE TABLE IF NOT EXISTS survey_answers (
     survey_answerID SERIAL PRIMARY KEY,
-    surveyID INT,
-    studentID INT,
+    surveyID INT NOT NULL,
+    studentID INT NOT NULL,
     FOREIGN KEY (surveyID) REFERENCES surveys(surveyID),
     FOREIGN KEY (studentID) REFERENCES students(studentID)
 );
 
 -- Création de la table answer_questions pour stocker les réponses aux questions
 CREATE TABLE IF NOT EXISTS answer_questions (
-    survey_answerID INT,
-    questionID INT,
-    answer_text TEXT,
+    survey_answerID INT NOT NULL,
+    questionID INT NOT NULL,
+    answer_text TEXT NOT NULL,
     FOREIGN KEY (questionID) REFERENCES questions(questionID),
     FOREIGN KEY (survey_answerID) REFERENCES survey_answers(survey_answerID),
     PRIMARY KEY (survey_answerID, questionID)
