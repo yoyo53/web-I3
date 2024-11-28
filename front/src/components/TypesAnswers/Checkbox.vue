63704762d8678dcd4c23973d5b16cf335c8a198d
Lucas Brancolini <lucas.brancolini@efrei.net>
2024-11-28 16:49:19 +0100

    added full form for template

--- front/src/components/TypesAnswers/Checkbox.vue
+++ front/src/components/TypesAnswers/Checkbox.vue
@@ -1,10 +1,5 @@
 <template>
-    <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-        <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-            <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-primary-blue-color">
-                Manage Checkboxes
-            </h2>
-        </div>
+    <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-6 lg:px-8">
 
         <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
             <div class="space-y-4">
