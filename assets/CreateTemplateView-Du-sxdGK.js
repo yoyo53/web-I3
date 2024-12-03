8c1b03654147f15af5efef27ee7dfdcea37c093d
yoyo53 <yoyo53@users.noreply.github.com>
2024-12-03 10:07:28 +0000

    deploy: 4b6e9387ce23d8432b057cd3197bac1d68232f91

--- assets/CreateTemplateView-Du-sxdGK.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c,o as t}from"./index-Dg90dZA0.js";const r={};function o(a,n){return t(),c("div")}const _=e(r,[["render",o]]);export{_ as default};
---
+++ assets/TemplateDetailView-BcOPqcE0.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o as t}from"./index-Be8YnV4o.js";const o={};function r(a,n){return t(),c("div")}const _=e(o,[["render",r]]);export{_ as default};
