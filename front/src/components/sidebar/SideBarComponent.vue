e94509ce1cb4bfb7d3c39304bb702896f29b9f03
Kurnoth <arnaud.kohler@bbox.fr>
2024-12-03 08:03:20 -0100

    Use userType in SideBard

--- front/src/components/sidebar/SideBarComponent.vue
+++ front/src/components/sidebar/SideBarComponent.vue
@@ -2,9 +2,9 @@
     <aside id="default-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
         <div class="h-full px-3 py-8 overflow-y-auto bg-gray-50 dark:bg-gray-800">
             <ul class="space-y-4 font-medium">
-                <TabComponent :img=ProfileSVG text="Profile"/>
-                <TabComponent :img=survey text="Surveys"/>
-                <TabComponent :img=template text="Template"/>
+                <TabComponent :img=ProfileSVG text="Profile" link="/profile"/>
+                <TabComponent :img=surveySVG text="Surveys" :link="userType === 'Admin' ? '/admin/survey' : userType === 'Teacher' ? '/teacher/survey' : '/student/survey'"/>
+                <TabComponent v-if="userType === 'Admin'" :img=templateSVG text="Template" link="/admin/template"/>
             </ul>
         </div>
     </aside>
@@ -13,19 +13,25 @@
 <script>
 import TabComponent from '@/components/sidebar/TabComponent.vue';
 import ProfileSVG from '@/assets/profile.svg';
-import survey from '@/assets/survey.svg';
-import template from '@/assets/template.svg';
+import surveySVG from '@/assets/survey.svg';
+import templateSVG from '@/assets/template.svg';
 
 export default {
     name: 'SideBarComponent',
+    props: {
+        userType: {
+            type: String,
+            required: true
+        }
+    },
     components: {
         TabComponent
     },
     data(){
         return {
             ProfileSVG,
-            survey,
-            template
+            surveySVG,
+            templateSVG,
         }
     }
 }
--- front/src/components/sidebar/TabComponent.vue
+++ front/src/components/sidebar/TabComponent.vue
@@ -1,6 +1,6 @@
 <template>
     <li>
-        <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+        <a :href="link" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
             <img :src="img" class="w-7 h-7 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true"/>
             <span class="ms-3 text-lg">{{ text }}</span>
         </a>
@@ -16,6 +16,10 @@ export default {
             required: true
         },
         text: {
+            type: String,
+            required: true
+        },
+        link: {
             type: String,
             required: true
         }
