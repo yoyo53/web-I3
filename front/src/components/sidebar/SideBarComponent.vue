a7933725f5b3b5cc0821b8b8f0f0dbfb658b18ef
Kurnoth <arnaud.kohler@bbox.fr>
2024-12-03 09:47:17 -0100

    Logout and routing

--- front/src/components/sidebar/SideBarComponent.vue
+++ front/src/components/sidebar/SideBarComponent.vue
@@ -1,10 +1,11 @@
 <template>
     <aside id="default-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
         <div class="h-full px-3 py-8 overflow-y-auto bg-gray-50 dark:bg-gray-800">
-            <ul class="space-y-4 font-medium">
-                <TabComponent :img=ProfileSVG text="Profile" link="/profile"/>
-                <TabComponent :img=surveySVG text="Surveys" :link="userState.userType === 'Admin' ? '/admin/survey' : userState.userType === 'Teacher' ? '/teacher/survey' : '/student/survey'"/>
-                <TabComponent v-if="userState.userType === 'Admin'" :img=templateSVG text="Template" link="/admin/template"/>
+            <ul class="space-y-4 font-medium h-full flex flex-col">
+                <TabComponent :img="ProfileSVG" text="Profile" :to="{ name: 'profile' }"/>
+                <TabComponent :img="surveySVG" text="Surveys" :to="getSurveyRoute"/>
+                <TabComponent v-if="userState.userType === 'Admin'" :img="templateSVG" text="Template" :to="{ name: 'AdminTemplate' }"/>
+                <TabComponent :img="logoutSVG" text="Logout" :to="{ name: 'login'}" textStyle="text-red-500 font-semibold" class="!mt-auto"/>
             </ul>
         </div>
     </aside>
@@ -15,6 +16,7 @@ import TabComponent from '@/components/sidebar/TabComponent.vue';
 import ProfileSVG from '@/assets/profile.svg';
 import surveySVG from '@/assets/survey.svg';
 import templateSVG from '@/assets/template.svg';
+import logoutSVG from '@/assets/logout.svg';
 
 export default {
     inject: ['userState'],
@@ -27,6 +29,24 @@ export default {
             ProfileSVG,
             surveySVG,
             templateSVG,
+            logoutSVG
+        }
+    },
+    computed: {
+        getSurveyRoute() {
+            if (this.userState.userType === 'Admin') {
+                return { name: 'admin' };
+            } else if (this.userState.userType === 'Teacher') {
+                return { name: 'teacher' };
+            } else {
+                return { name: 'StudentSurvey' };
+            }
+        }
+    },
+    methods: {
+        logout() {
+            localStorage.removeItem('token');
+            localStorage.removeItem('userType');
         }
     }
 }
--- front/src/components/sidebar/TabComponent.vue
+++ front/src/components/sidebar/TabComponent.vue
@@ -1,9 +1,9 @@
 <template>
-    <li>
-        <a :href="link" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+    <li @click="handleClick">
+        <router-link :to="to" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
             <img :src="img" class="w-7 h-7 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true"/>
-            <span class="ms-3 text-lg">{{ text }}</span>
-        </a>
+            <span :class="['ms-3', 'text-lg', textStyle]">{{ text }}</span>
+        </router-link>
     </li>
 </template>
 
@@ -19,9 +19,18 @@ export default {
             type: String,
             required: true
         },
-        link: {
+        click: {
+            type: Function,
+            default: () => {}
+        },
+        textStyle: {
             type: String,
-            required: true
+            default: 'text-white'
+        },
+    },
+    methods: {
+        handleClick() {
+            this.click();
         }
     }
 }
