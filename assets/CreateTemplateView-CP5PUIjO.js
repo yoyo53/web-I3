0b63b8333378dce87fcda3d951a6c3695d8c8648
ArnaudKohler <ArnaudKohler@users.noreply.github.com>
2024-12-03 09:36:18 +0000

    deploy: 6aabbca4c482db031068594a8804a5074e0cc2a8

--- assets/CreateTemplateView-CP5PUIjO.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c,o as t}from"./index-CHaC3lXE.js";const r={};function o(a,n){return t(),c("div")}const _=e(r,[["render",o]]);export{_ as default};
---
+++ assets/TemplateDetailView-DI4Kwuz_.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o as t}from"./index-ANTf7xIC.js";const o={};function r(a,n){return t(),c("div")}const _=e(o,[["render",r]]);export{_ as default};
