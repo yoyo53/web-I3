3884124280130f00f31e7590c99b5467f8aca69b
Lucas Brancolini <lucas.brancolini@efrei.net>
2024-12-04 16:55:53 +0100

    linked back and front template creation

--- back/controllers/admin.controller.js
+++ back/controllers/admin.controller.js
@@ -19,8 +19,18 @@ async function getSurveyTemplates(req, res) {
     }
 }
 
+async function createSurveyTemplate(req, res) {
+    const response = await templateQueries.createSurveyTemplate(req.body.name, req.body.questions)
+    if (response != null) {
+        res.status(200).json(response);
+    } else {
+        res.status(500).send('Error while fetching templates');
+    }
+};
+
 
 module.exports = {
     getAdminSurveys,
     getSurveyTemplates,
+    createSurveyTemplate
 };
