b628b33b474335dd8d5c84aa2ac5c2ef1c38a15a
Hugo Parmentier <hugo.parmentier@efrei.net>
2024-12-04 13:41:29 +0100

    colors fix + login fix and register

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -11,20 +11,12 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-    {
       path: '/login',
       name: 'login',
       component: () => import('../views/LoginView.vue'),
     },
     {
-      path: '/register',
+      path: '/admin/register',
       name: 'register',
       component: () => import('../views/RegisterView.vue'),
     },
@@ -64,6 +56,7 @@ router.beforeEach(async (to, from, next) => {
   const userState = inject('userState');
   userState.userType = null;
   userState.userId = null;
+  //localStorage.removeItem('token');
   if (!authRequired) {
     next();
   }
--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -5,28 +5,32 @@ export default {
     "./App.vue",
   ],
   theme: {
-    colors: {
-      'primary': '#163767', // Text
-      'primary-hover':'#1e4885', //1e4885 040c3c
+    extend: {
+      colors: {
+        'primary': '#163767', // Text
+        'primary-hover': '#1e4885', //1e4885 040c3c
 
-      'secondary': '#3653a0', 
-      'tertiary': '#377fbc',  
+        'secondary': '#3653a0',
+        'tertiary': '#377fbc',
 
-      'delete': '#d11a2a',
+        'delete': '#d11a2a',
 
-      'red-50': "#FEF2F2",
-      'red-100': "#FEE2E2",
-      'red-500': "#EF4444",
-      'red-700': "#B91C1C",
+        'red-50': "#FEF2F2",
+        'red-100': "#FEE2E2",
+        'red-500': "#EF4444",
+        'red-700': "#B91C1C",
 
-      'transparent': '#ecf2f7',
-      'transparent-hover': '#dce6f4',
+        'transparent': '#ecf2f7',
+        'transparent-hover': '#dce6f4',
 
-      'electric-blue':'#2566e8',
-      'purple-color': '#95569e',
-      'orange-color': '#e7873b',
-      'red-color': '#dd5555',
-   },
+        'light-blue': '#9dc0f9',
+
+        'electric-blue': '#2566e8',
+        'purple-color': '#95569e',
+        'orange-color': '#e7873b',
+        'red-color': '#dd5555',
+      },
+    }
   },
   plugins: [],
 }
