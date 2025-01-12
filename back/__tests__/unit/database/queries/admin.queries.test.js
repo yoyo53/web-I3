0bac0523046dc811fd42734d8910891092c3ab1a
tonelomb <lucas.brancolini@efrei.net>
2025-01-12 16:30:33 +0100

    Added some unit tests

--- back/__tests__/unit/database/queries/admin.queries.test.js
+++ back/__tests__/unit/database/queries/admin.queries.test.js
@@ -20,3 +20,66 @@ describe("Check exists admin", () => {
     expect(response).toBe(false);
   });
 });
+
+describe("Check All surveys", () => {
+  it("should return all surveys", async () => {
+    const surveys = [
+      {
+        surveyid: 1,
+        teachers: {
+          users: {
+            lastname: "Doe",
+            firstname: "John"
+          }
+        },
+        modules: {
+          subjects:{
+            name: "Mathematics"
+          },
+          groups:{
+            name: "SE1"
+          }
+        }
+      },
+      {
+        surveyid: 2,
+        teachers: {
+          users: {
+            lastname: "Smith",
+            firstname: "John"
+          }
+        },
+        modules: {
+          subjects:{
+            name: "Physics"
+          },
+          groups:{
+            name: "SE2"
+          }
+        }
+      }
+    ]
+    prismaMock.surveys.findMany.mockResolvedValue(surveys);
+    const response = await adminQueries.getAllSurveys();
+    expect(response).toEqual(surveys.map(survey => ({
+      surveyid: survey.surveyid,
+      lastname: survey.teachers.users.lastname,
+      firstname: survey.teachers.users.firstname,
+      subject: survey.modules.subjects.name,
+      group: survey.modules.groups.name
+    })))
+
+  });
+
+  it("should return empty list if no surveys", async () => {
+    prismaMock.surveys.findMany.mockResolvedValue([])
+    const response = await adminQueries.getAllSurveys();
+    expect(response).toEqual([])
+  });
+
+   it("should return null if an error occurs", async () => {
+      prismaMock.surveys.findMany.mockRejectedValue(new Error());
+      const response = await adminQueries.getAllSurveys();
+      expect(response).toBeNull();
+    });
+})
