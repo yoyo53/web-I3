0b52b764c05dabd7064f71b2eef1f23efcac1978
ludovic <ludovic.liu.chi.pioa@efrei.net>
2025-01-14 08:33:19 +0100

    added redirect to student survey

--- front/src/components/surveys/SurveysListComponent.vue
+++ front/src/components/surveys/SurveysListComponent.vue
@@ -7,9 +7,18 @@
           :key="survey.id" 
           class="border border-gray-300 rounded-lg p-4 bg-gray-50 transition-transform duration-200 ease-in-out hover:bg-gray-100 hover:-translate-y-1 hover:shadow-md cursor-pointer"
           >
-          <router-link :to="{ name: 'teacher-survey', params: { id: survey.surveyid } }">
-            <SurveyComponent :survey="survey" />
-          </router-link>  
+          <router-link 
+          v-if="userState.userType === 'Student'" 
+          :to="{ name: 'student-survey', params: { id: survey.surveyid } }"
+        >
+          <SurveyComponent :survey="survey" />
+        </router-link>
+        <router-link 
+          v-else-if="userState.userType === 'Teacher'" 
+          :to="{ name: 'teacher-survey', params: { id: survey.surveyid } }"
+        >
+          <SurveyComponent :survey="survey" />
+        </router-link> 
         </li>
       </ul>
     </div>
@@ -20,7 +29,8 @@
     export default{
         components: {
             SurveyComponent,
-        },
+        }, 
+        inject: ['userState'],
         props: {
         surveys: {
             type: Array,
