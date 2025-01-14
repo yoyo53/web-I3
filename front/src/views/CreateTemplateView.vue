4d83fbba07a42275aef67b666b439f1737b6ede2
Hugo Parmentier <hugo.parmentier@efrei.net>
2025-01-14 10:15:06 +0100

    supp min-h-screen

--- front/src/views/CreateTemplateView.vue
+++ front/src/views/CreateTemplateView.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="min-h-screen p-8 flex justify-center items-start">
+    <div class="p-8 flex justify-center items-start">
       <div class="bg-white shadow-md rounded-lg p-6 w-full max-w-3xl">
         <h1 class="text-2xl font-semibold text-[primary] mb-4">Create Template</h1>
         <CreateTemplateComponent/>
