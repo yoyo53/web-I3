db333ead162e09686d9d6a072e0c948c36b0b46e
ludovic <ludovic.liu.chi.pioa@efrei.net>
2025-01-15 16:35:37 +0100

    added unit test for HomeView.vue

--- front/__tests__/views/HomeView.test.js
+++ front/__tests__/views/HomeView.test.js
@@ -1,11 +1,35 @@
-import { test } from 'vitest'
+import { test, describe } from 'vitest'
 import { render } from '@testing-library/vue'
 import HomeView from '../../src/views/HomeView.vue'
 
-test('Page', () => {
-  const page = render(HomeView, {
-    props: {
-    }
+describe('HomeView.vue', () => {
+  test('renders the main heading', () => {
+    const { getAllByText } = render(HomeView)
+    getAllByText('TeachPoint. Teacher grading made easy')
   })
-  page.getAllByText('TeachPoint. Teacher grading made easy')
+
+  test('renders the description paragraph', () => {
+    const { getAllByText } = render(HomeView)
+    getAllByText('TeachPoint is a platform that helps students grade their teachers easily and efficiently. With TeachPoint, students can grade their teachers in a few clicks.')
+  })
+
+  test('renders the "Learn more" button', () => {
+    const { getAllByText } = render(HomeView)
+    getAllByText('Learn more')
+  })
+
+  test('renders the "Watch video" button', () => {
+    const { getAllByText } = render(HomeView)
+    getAllByText('Watch video')
+  })
+
+  test('renders the "They trust us" section', () => {
+    const { getAllByText } = render(HomeView)
+    getAllByText('They trust us')
+  })
+
+  test('renders the "Let\'s get started" section', () => {
+    const { getAllByText } = render(HomeView)
+    getAllByText('Let\'s get started')
+  })
 })
